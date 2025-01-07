1dc82571cf52dc71598e31b9f2bc6e1a1440861c
danicanhackit <danih8072007@gmail.com>
2025-01-07 14:25:43 -0600

    fix keyboard problem

--- app/src/main/java/com/example/scout/AddReport.kt
+++ app/src/main/java/com/example/scout/AddReport.kt
@@ -1,7 +1,6 @@
 package com.example.scout
 
-import android.util.Log
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,29 +9,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.testing.TestNavHostController
-import com.example.scout.api.RetrofitInstance
-import com.example.scout.api.TeamResponse
+import com.example.scout.ui.theme.Burgundy
+import com.example.scout.ui.theme.PlatyRed
 import com.example.scout.ui.theme.ScoutTheme
-import com.example.scout.ui.theme.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 private const val API_KEY = "lYGojKcODnYEUfpa486Fs0Z8oYI9R2TkS3RS6m3qc39PS43SOB3MxVwS2OZtB7Mf"
@@ -41,6 +46,7 @@ private const val API_KEY = "lYGojKcODnYEUfpa486Fs0Z8oYI9R2TkS3RS6m3qc39PS43SOB3
 @Composable
 fun AddReport(navController: NavHostController){
     var teamNum by remember { mutableStateOf(TextFieldValue(" "))}
+    val keyboardController = LocalSoftwareKeyboardController.current
     Column {
         CenterAlignedTopAppBar(
             title = {
@@ -68,6 +74,10 @@ fun AddReport(navController: NavHostController){
                 onValueChange = { teamNum = it },
                 label = { Text("Team Being Scouted", color = Burgundy)},
                 textStyle = TextStyle(color = Burgundy),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide()}
+                ),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = PlatyRed,
                     unfocusedBorderColor = Burgundy,
--- app/src/main/java/com/example/scout/SignIn.kt
+++ app/src/main/java/com/example/scout/SignIn.kt
@@ -2,22 +2,39 @@ package com.example.scout
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.testing.TestNavHostController
-import com.example.scout.api.BlueAllianceAPI
 import com.example.scout.api.RetrofitInstance
 import com.example.scout.api.TeamResponse
 import com.example.scout.ui.theme.Burgundy
@@ -37,6 +54,7 @@ fun SignIn(teamViewModel: TeamViewModel, navController: NavHostController) {
     var scouterName by remember { mutableStateOf(TextFieldValue("")) }
     var teamNumber by remember { mutableStateOf(TextFieldValue("")) }
     val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
     //val teamViewModel: TeamViewModel = viewModel()
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -60,7 +78,11 @@ fun SignIn(teamViewModel: TeamViewModel, navController: NavHostController) {
                 onValueChange = { teamNumber = it },
                 label = { Text("Team Number:", color = Burgundy) },
                 textStyle = TextStyle(color = Burgundy),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                // keyboard controller code from that one resource i put in a google doc
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide()}
+                ),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = PlatyRed,
                     unfocusedBorderColor = Burgundy,
@@ -75,6 +97,10 @@ fun SignIn(teamViewModel: TeamViewModel, navController: NavHostController) {
                 onValueChange = { scouterName = it },
                 label = { Text("Scouter Name:", color = Burgundy) },
                 textStyle = TextStyle(color = Burgundy),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide()}
+                ),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = PlatyRed,
                     unfocusedBorderColor = Burgundy,
